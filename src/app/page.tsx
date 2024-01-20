bf23fbfb76db8218c5d6f30d74101c6e708def0d
Thiago de Andrade Feliciano <thiagoandf@gmail.com>
2024-01-20 16:19:46 +0100

    create t3-app

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+
+export default function HomePage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center text-white">
+      <div className="container flex flex-col items-center justify-center">
+      </div>
+    </main>
+  );
+}
---
+++ src/layouts/lilly58.ts
@@ -0,0 +1,210 @@
+export const lilly58 = [
+  // Row 1 - 12 keys
+  [
+    {
+      type: "1u",
+    },
+    {
+      type: "1u",
+    },
+    {
+      type: "1u",
+    },
+    {
+      type: "1u",
+    },
+    {
+      type: "1u",
+    },
+    {
+      type: "1u",
+    },
+    {
+      type: "1u",
+    },
+    {
+      type: "1u",
+    },
+    {
+      type: "1u",
+    },
+    {
+      type: "1u",
+    },
+    {
+      type: "1u",
+    },
+    {
+      type: "1u",
+    },
+  ],
+  // Row 2 - 12 keys
+  [
+    {
+      type: "1u",
+    },
+    {
+      type: "1u",
+    },
+    {
+      type: "1u",
+    },
+    {
+      type: "1u",
+    },
+    {
+      type: "1u",
+    },
+    {
+      type: "1u",
+    },
+    {
+      type: "1u",
+    },
+    {
+      type: "1u",
+    },
+    {
+      type: "1u",
+    },
+    {
+      type: "1u",
+    },
+    {
+      type: "1u",
+    },
+    {
+      type: "1u",
+    },
+  ],
+  // Row 3 - 14 keys
+  [
+    {
+      type: "1u",
+    },
+    {
+      type: "1u",
+    },
+    {
+      type: "1u",
+    },
+    {
+      type: "1u",
+    },
+    {
+      type: "1u",
+    },
+    {
+      type: "1u",
+    },
+    {
+      type: "1u",
+    },
+    {
+      type: "1u",
+    },
+    {
+      type: "1u",
+    },
+    {
+      type: "1u",
+    },
+    {
+      type: "1u",
+    },
+    {
+      type: "1u",
+    },
+    {
+      type: "1u",
+    },
+    {
+      type: "1u",
+    },
+  ],
+
+  // Row 4 - 12 keys
+  [
+    {
+      type: "1u",
+    },
+    {
+      type: "1u",
+    },
+    {
+      type: "1u",
+    },
+    {
+      type: "1u",
+    },
+    {
+      type: "1u",
+    },
+    {
+      type: "1u",
+    },
+    {
+      type: "1u",
+    },
+    {
+      type: "1u",
+    },
+    {
+      type: "1u",
+    },
+    {
+      type: "1u",
+    },
+    {
+      type: "1u",
+    },
+    {
+      type: "1u",
+    },
+  ],
+  // Row 5 - 8 keys
+  [
+    {
+      type: "blank",
+    },
+    {
+      type: "blank",
+    },
+    {
+      type: "blank",
+    },
+    {
+      type: "1u",
+    },
+    {
+      type: "1u",
+    },
+    {
+      type: "1u",
+    },
+    {
+      type: "1.5u",
+    },
+    {
+      type: "1.5u",
+    },
+    {
+      type: "1u",
+    },
+    {
+      type: "1u",
+    },
+    {
+      type: "1u",
+    },
+    {
+      type: "blank",
+    },
+    {
+      type: "blank",
+    },
+    {
+      type: "blank",
+    },
+  ]
+]
